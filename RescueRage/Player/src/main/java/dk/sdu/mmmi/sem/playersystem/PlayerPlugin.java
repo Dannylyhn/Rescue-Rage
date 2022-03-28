b8d335c9e300e0d333ee7c53da084a9ac1f55544
HotdogChamp <dangu19@student.sdu.dk>
2022-03-28 12:01:26 +0200

    Added player.

---
+++ RescueRage/Player/src/main/java/dk/sdu/mmmi/sem/playersystem/PlayerPlugin.java
@@ -0,0 +1,54 @@
+package dk.sdu.mmmi.sem.playersystem;
+
+import dk.sdu.mmmi.sem.common.data.Entity;
+import dk.sdu.mmmi.sem.common.data.GameData;
+import dk.sdu.mmmi.sem.common.data.World;
+import dk.sdu.mmmi.sem.common.data.entityparts.LifePart;
+import dk.sdu.mmmi.sem.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.sem.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.sem.common.services.IGamePluginService;
+import org.openide.util.lookup.ServiceProvider;
+import org.openide.util.lookup.ServiceProviders;
+
+@ServiceProviders(value = {
+    @ServiceProvider(service = IGamePluginService.class),})
+public class PlayerPlugin implements IGamePluginService {
+
+    private Entity player;
+
+    public PlayerPlugin() {
+    }
+
+    @Override
+    public void start(GameData gameData, World world) {
+
+        // Add entities to the world
+        player = createPlayerShip(gameData);
+        world.addEntity(player);
+    }
+
+    private Entity createPlayerShip(GameData gameData) {
+
+        float deacceleration = 10;
+        float acceleration = 200;
+        float maxSpeed = 300;
+        float rotationSpeed = 5;
+        float x = gameData.getDisplayWidth() / 2;
+        float y = gameData.getDisplayHeight() / 2;
+        float radians = 3.1415f / 2;
+
+        Entity playerShip = new Player();
+        playerShip.setRadius(8);
+        playerShip.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
+        playerShip.add(new PositionPart(x, y, radians));
+
+        return playerShip;
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        // Remove entities
+        world.removeEntity(player);
+    }
+
+}
