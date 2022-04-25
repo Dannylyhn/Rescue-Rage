2e10f837b704efd50d137a873b0eef73991b447d
Danny Ly <dannylyhn@gmail.com>
2022-04-25 12:25:41 +0200

    Ok

---
+++ RescueRage/Common/src/main/java/rescuerage/common/data/entityparts/EnemyMovingPart.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rescuerage.common.data.entityparts;
+
+import rescuerage.common.data.Entity;
+import rescuerage.common.data.GameData;
+import static rescuerage.common.data.GameKeys.A;
+import static rescuerage.common.data.GameKeys.D;
+import static rescuerage.common.data.GameKeys.W;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
+
+/**
+ *
+ * @author Alexander
+ */
+public class EnemyMovingPart
+        implements EntityPart {
+
+    private float dx, dy;
+    private float deceleration, acceleration;
+    private float maxSpeed, rotationSpeed;
+    private boolean left, right, up, down;
+
+    public EnemyMovingPart(float maxSpeed) {
+       
+        this.maxSpeed = maxSpeed;
+       
+    }
+
+    public float getDx() {
+        return dx;
+    }
+
+    public float getDy() {
+        return dy;
+    }
+
+    public void setDeceleration(float deceleration) {
+        this.deceleration = deceleration;
+    }
+
+    public void setAcceleration(float acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public void setMaxSpeed(float maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void setSpeed(float speed) {
+        this.acceleration = speed;
+        this.maxSpeed = speed;
+    }
+
+    public void setRotationSpeed(float rotationSpeed) {
+        this.rotationSpeed = rotationSpeed;
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+    
+    public void setDown(boolean down){
+        this.down = down;
+    }
+
+    @Override
+    public void process(GameData gameData, Entity entity) {
+        PositionPart positionPart = entity.getPart(PositionPart.class);
+        float x = positionPart.getX();
+        float y = positionPart.getY();
+        float radians = positionPart.getRadians();
+        float dt = gameData.getDelta();
+
+        // turning
+        if (left) {
+            dx = -maxSpeed;
+        }
+
+        if (right) {
+            dx = maxSpeed;
+        }
+
+        // accelerating            
+        if (up) {
+            dy =  maxSpeed;
+        }
+        
+        if (down){
+            dy = -maxSpeed;
+        }
+
+        // set position
+        x += dx * dt;
+        if (x > gameData.getDisplayWidth()) {
+            x = 0;
+        } else if (x < 0) {
+            x = gameData.getDisplayWidth();
+        }
+
+        y += dy * dt;
+        if (y > gameData.getDisplayHeight()) {
+            y = 0;
+        } else if (y < 0) {
+            y = gameData.getDisplayHeight();
+        }
+
+        positionPart.setX(x);
+        positionPart.setY(y);
+
+        positionPart.setRadians(radians);
+    }
+
+}
---
+++ RescueRage/Enemy/src/main/java/rescuerage/enemysystem/Enemy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rescuerage.enemysystem;
+import rescuerage.common.data.entityparts.EnemyMovingPart;
+import rescuerage.common.data.Entity;
+
+
+/**
+ *
+ * @author danny
+ */
+public class Enemy extends Entity{
+    
+}
