c3cef92b1a8a9b98746ff457f7551658021d74f4
Danny Ly <dannylyhn@gmail.com>
2022-05-25 13:32:17 +0200

    Updated test module

--- RescueRage/Testing/src/test/java/collision/CollisionTest.java
+++ RescueRage/Testing/src/test/java/collision/CollisionTest.java
@@ -4,10 +4,42 @@
  */
 package collision;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import rescuerage.common.data.Entity;
+import rescuerage.common.data.GameData;
+import rescuerage.common.data.World;
+import rescuerage.playersystem.PlayerPlugin;
+
 /**
  *
  * @author danny
  */
 public class CollisionTest {
+
+    PlayerPlugin playerplugin;
+    GameData gamedata;
+    World world;
+    Entity testPlayer;
+    float playerX;
+    float playerY;
     
+
+    
+    @BeforeEach
+    public void setUp(){
+    playerplugin = new PlayerPlugin();
+    gamedata = new GameData();
+    world = new World();
+    }
+    
+    
+    @Test
+    @DisplayName("Collision testing")
+    public void CollisionTest() {
+        
+        assertNotNull("penis");
+    }
 }
